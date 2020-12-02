35b2efe1acb71d7c1b87be0f2fa5bedbc3ad1128
Anotnio the Gull <abenfante97@gmail.com>
2020-12-02 14:51:15 +0100

    Volo migliorato, ripuliti un po' di file inutili

---
+++ Assets/Scripts/ModelYawAndPitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelYawAndPitch : MonoBehaviour //QUESTA CLASSE RUOTA IL MODELLO IN BASE AI MOVIMENTI DEL RIGIDBODY QUANDO SI è IN ARIA
+{
+    public Rigidbody rb;
+    private bool isGrounded = false;
+    public float pitchSensitivity = 1f;
+    public float rollSensitivity = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(DeltaHeightToPitch(), transform.eulerAngles.y, DeltaYawToRoll());
+    }
+
+    private float DeltaHeightToPitch()
+    {
+       return rb.velocity.y * pitchSensitivity * -1f;
+    }
+
+    private float DeltaYawToRoll()
+    {
+        return rb.angularVelocity.y * rollSensitivity * -1f;
+    }
+}
---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    public Vector3 windForce = Vector3.zero;
+
+    private void Update()
+    {
+        Debug.DrawRay(transform.position, windForce);
+    }
+
+
+}
