d3cfee112926eebd020ede58fa3984041745ecb4
fede-2024 <72552157+fede-2024@users.noreply.github.com>
2020-12-28 22:56:29 +0100

    prototipo cinemachine

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            Switch();
+    }
+
+    public void Switch()
+    {
+        if (_animator == null)
+            return;
+
+        else if (_animator.GetBool("switch") == false)
+        _animator.SetBool("switch", true);
+
+        else if (_animator.GetBool("switch") == true)
+            _animator.SetBool("switch", false);
+    }
+}
---
+++ Assets/Scripts/SwitchCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    [SerializeField] public CinemachineVirtualCamera vcam1;
+    [SerializeField] public CinemachineVirtualCamera vcam2;
+    [SerializeField] public bool _switchNow;
+
+    public float time = 1;
+
+    void Update()
+    {
+        if(_switchNow)
+        {
+            var brain = GetComponent<CinemachineBrain>();
+            brain.m_DefaultBlend.m_Time = time;
+
+            if (brain.IsLive(vcam1))
+            {
+                vcam2.gameObject.SetActive(true);
+                vcam1.gameObject.SetActive(false);
+            }
+            else
+            {
+                vcam2.gameObject.SetActive(false);
+                vcam1.gameObject.SetActive(true);
+            }
+            _switchNow = false;
+        }
+    }
+}
